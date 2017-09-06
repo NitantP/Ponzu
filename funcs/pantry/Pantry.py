7b9c768f24c5ed7efaf7616fc526a0d45e17aa6c
NitantP <patel.nitant.bca@gmail.com>
2017-09-05 21:48:06 -0400

    Added updates and search functions to pantry, tidied up output, included success messages, TODO: formatting

--- funcs/pantry/Pantry.py
+++ funcs/pantry/Pantry.py
@@ -15,15 +15,33 @@ class Pantry:
         """Used to print contents of the pantry."""
         print("Size of pantry: " + str(self.size) + "\n")
         for iname, attr in self.pantry.items():
-            print(iname + "\t" + attr[0] + " " + attr[1] + " " + attr[2] + "\n")
+            print(iname + "\t" + attr[0] + " " + attr[1] + " " + attr[2])
 
     def add_item(self, name, purchase_date, expiration_date, quantity):
         if name in self.pantry:
-            print("\nItem is already in the pantry. Try updating it instead!\n")
+            print("\nItem is already in the pantry. Try updating it instead!")
         else:
             self.pantry[name] = [purchase_date, expiration_date, quantity]
             self.size += 1
+            return "\n" + name + ": [" + purchase_date + ", " + expiration_date + ", " + quantity + "] -- item succesfully added!"
     
     def save(self):
         with open("pantry.json", "w") as myPantry:
             json.dump(self.pantry, myPantry, indent=4, sort_keys=True)
+        return "Pantry succesfully saved!"
+    
+    def search(self, iname):
+        attr = self.pantry[iname]
+        return iname + " " + attr[0] + " " + attr[1] + " " + attr[2]
+
+    def update(self, new_list):
+        old_attr = self.pantry[new_list[0]]
+        if new_list[1] == "purchase date":
+            old_attr[0] = new_list[2]
+        elif new_list[1] == "expiration date":
+            old_attr[1] = new_list[2]
+        elif new_list[1] == "quantity":
+            old_attr[2] = new_list[2]
+        self.pantry[new_list[0]] = old_attr
+
+        return "Update completed (" + new_list[1] + " --> " + new_list[2] + ")"
--- funcs/pantry/pantrymenu.py
+++ funcs/pantry/pantrymenu.py
@@ -10,16 +10,17 @@ try:
         print("-- Found an existing file!\n")
 except:
     pantry = Pantry.Pantry()
-    print("-- No existing file found, here's a fresh pantry!\n")
+    print("-- No existing file found. Here's a fresh pantry!\n")
 
-pantrynav = -1
-
-while int(pantrynav) < 3:
+while True:
     print("[0] Print current pantry\n")
     print("[1] Add item to pantry\n")
     print("[2] Save pantry\n")
+    print("[3] Update pantry item\n")
+    print("[4] Search pantry for item\n")
+    print("[q] Exit pantry system\n")
 
-    pantrynav = input("Choose an option (number) from above: ")
+    pantrynav = input("Choose an option from above: ")
     print("===========================================")
 
     if pantrynav == '0':
@@ -29,11 +30,21 @@ while int(pantrynav) < 3:
         ipdate = input("Purchase date: ")
         iedate = input("Expiration date: ")
         iquantity = input("Quantity: ")
-        pantry.add_item(iname, ipdate, iedate, iquantity)
+        print(pantry.add_item(iname, ipdate, iedate, iquantity))
     elif pantrynav == '2':
-        pantry.save()
+        print(pantry.save())
+    elif pantrynav == '3':
+        new_attr = []
+        print("Enter the name of item to update and information to update")
+        new_attr.append(input("Name: "))
+        new_attr.append(input("Attribute: "))
+        new_attr.append(input("Value: "))
+        print(pantry.update(new_attr))
+    elif pantrynav == '4':
+        print(pantry.search(input("Name: ")))
+    elif pantrynav == 'q':
         sys.exit()
     else:
-        print("Try again")
+        print("Invalid choice")
 
     print("===========================================")
